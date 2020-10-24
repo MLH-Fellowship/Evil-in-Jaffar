b4faf0fa13c10defd6caab251b02766f4e27b524
V2dha <rvividha@gmail.com>
2020-10-24 18:58:39 +0530

    added new scene bandits

---
+++ Fantasy Game/Assets/bandit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class bandit : MonoBehaviour
+
+// Code taken from https://www.youtube.com/watch?v=jvtFUfJ6CP8
+
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+	// Change face direction of enemy when player goes in another direction
+        if(aiPath.desiredVelocity.x >= 0.01f) {
+		transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+	}
+	else if (aiPath.desiredVelocity.x <= -0.01f) {
+		transform.localScale = new Vector3(-0.6f, 0.6f, 0.6f);
+	}
+    }
+}
